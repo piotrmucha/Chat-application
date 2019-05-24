bd645097168d3491563da8f2d8845738ea1c98c3
Piotrold <35507293+Piotrold@users.noreply.github.com>
2019-05-24 15:21:39 +0200

    Login set

---
+++ src/client/LogController.java
@@ -0,0 +1,63 @@
+package client;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+
+
+public class LogController  {
+    @FXML private TextField username;
+    final static int ServerPort = 4999;
+    public LogController(){}
+    public void initialize () {
+
+        Task task = new Task<Void>() {
+            @Override
+            public Void call() {
+                try {
+                    System.out.println(Thread.currentThread().getName());
+                    //  Thread.sleep(1000);
+                    InetAddress ip = InetAddress.getByName("10.220.3.36");
+
+                    // establish the connection
+
+                    Socket s = new Socket(ip, ServerPort);
+
+                    // obtaining input and out streams
+
+                    DataInputStream dis = new DataInputStream(s.getInputStream());
+                    DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                    int b = 0;
+                    //    } catch (InterruptedException e) {
+                    //       Thread.currentThread().interrupt();
+                    // code for stopping current task so thread stops
+
+                } catch (IOException e) {
+                    int k = 0;
+                }
+                return null;
+            }
+        };
+        new Thread(task).start();
+
+
+
+
+
+    }
+    @FXML
+    void loginClick() {
+        String user = username.getText();
+        int k = 0;
+    }
+}
--- src/client/Main.java
+++ src/client/Main.java
@@ -1,24 +1,43 @@
 package client;
 
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Main extends Application {
+    static LogController myControllerHandle;
+    static AnchorPane rootPane;
+    static Scene scene;
+    final static int ServerPort = 4999;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("Hello World");
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+      //  myControllerHandle = new LogController();
+        //loader.setController(myControllerHandle);
+        Parent root = loader.load();
         Scene scene = new Scene(root);
+        primaryStage.setTitle("Okno logowania");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+
+
     }
 }
