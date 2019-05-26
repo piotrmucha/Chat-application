48129c39541c35445d6fdf9cb5af18e78c454fb1
Piotrold <35507293+Piotrold@users.noreply.github.com>
2019-05-26 17:41:01 +0200

    Change in logController and login.fxml set few gui things

--- src/client/LogController.java
+++ src/client/LogController.java
@@ -1,15 +1,15 @@
 package client;
-
-import javafx.application.Application;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
+import javafx.scene.control.TextField;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-
-import java.awt.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -17,17 +17,25 @@ import java.net.Socket;
 
 public class LogController  {
     @FXML private TextField username;
+    @FXML private Button click;
+    @FXML private TextFlow status;
     final static int ServerPort = 4999;
     public LogController(){}
     public void initialize () {
-
+       // Platform.runLater(()-> {
+        Text wait = new Text("Czekam na połączenie..");
+        status.getChildren().add(wait);
+        status.setTextAlignment(TextAlignment.CENTER);
+        username.setDisable(true);
+        click.setDisable(true);
+      //  });
         Task task = new Task<Void>() {
             @Override
             public Void call() {
                 try {
-                    System.out.println(Thread.currentThread().getName());
+
                     //  Thread.sleep(1000);
-                    InetAddress ip = InetAddress.getByName("10.220.3.36");
+                    InetAddress ip = InetAddress.getByName("192.168.0.15");
 
                     // establish the connection
 
@@ -42,13 +50,27 @@ public class LogController  {
                     //       Thread.currentThread().interrupt();
                     // code for stopping current task so thread stops
 
-                } catch (IOException e) {
-                    int k = 0;
+                } catch (Exception e) {
+                    Platform.runLater(()-> {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Error");
+                        alert.setHeaderText(null);
+                        alert.setContentText("Nie udało się nawiązac połączenia z serwerem. Sprawdź swoje połączenie internetowe.");
+                        alert.showAndWait();
+                        Platform.exit();
+                        System.exit(0);
+                    });
+
                 }
+                status.getChildren().clear();
+                Text correct = new Text("Pomyślnie nawiązano połączenie z serwerem");
+                status.getChildren().add(correct);
                 return null;
             }
         };
-        new Thread(task).start();
+        Thread connection = new Thread(task);
+        connection.setDaemon(true);
+        connection.start();
 
 
 
